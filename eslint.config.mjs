7fd067793a674b9b722c08957727e62ba3cce8d3
JooKangSan <you04045@naver.com>
2025-04-02 04:44:30 +0900

    feat: husky 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,75 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import parser from "@typescript-eslint/parser";
+import plugin from "@typescript-eslint/eslint-plugin";
+import storybook from "eslint-plugin-storybook";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      import: importPlugin,
+      "@typescript-eslint": plugin,
+      storybook,
+    },
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: {
+      react: { version: "detect" },
+      "import/resolver": {
+        typescript: {},
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+    rules: {
+      "import/no-extraneous-dependencies": "off",
+      "no-console": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/naming-convention": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: false,
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+      "react-hooks/rules-of-hooks": "off",
+      "import/extensions": "off",
+      "prefer-const": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/prop-types": "off",
+      "jsx-a11y/label-has-associated-control": [
+        "error",
+        {
+          required: {
+            some: ["nesting", "id"],
+          },
+        },
+      ],
+      "react/require-default-props": "off",
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+      // ✅ Storybook 관련 룰 추천 적용 (원하면 더 추가 가능)
+      ...storybook.configs.recommended.rules,
+    },
+  },
 ];
-
-export default eslintConfig;
