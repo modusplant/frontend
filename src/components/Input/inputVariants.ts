8eeb0feb9f2d7ad00aee8886f0adc91766d7e386
JooKangSan <you04045@naver.com>
2025-04-09 02:26:34 +0900

    MP-131-MP-130-refactor: 컬러 코드 수정 및 코드 테스트

--- src/components/Input/inputVariants.ts
+++ src/components/Input/inputVariants.ts
@@ -2,19 +2,20 @@
 import { VariantProps, cva } from "class-variance-authority";
 
 export const inputVariants = cva(
-  "w-full max-w-[90%] h-[47px] px-[18px] py-[17px] text-left text-[18px] mb-[16px] rounded-md border outline-none transition-all " +
+  "w-full h-[47px] px-[18px] py-[17px] text-left text-[18px] mb-[16px] rounded-md border-[2px] outline-none transition-all " +
     "tablet:max-w-full tablet:h-[45px] tablet:px-[18px] tablet:py-[16px] tablet:mb-[12px] tablet:text-[18px] " +
     "desktop:h-[44px] desktop:px-[18px] desktop:py-[16px] desktop:mb-[10px] desktop:text-[16px]",
   {
     variants: {
       status: {
-        regular: "border-gray-400 text-black",
-        active: "border-green-500 ring-1 ring-green-200",
-        hover: "border-green-500",
-        error: "border-red-400 text-red-600",
-        warning: "border-yellow-400 text-yellow-700",
-        valid: "border-green-500 text-green-700",
-        disabled: "border-gray-300 text-gray-400 bg-gray-100 cursor-not-allowed",
+        regular: "border-neutral-900 text-black",
+        active: "border-primary-500 shadow-[2px_2px_6px_0px_rgba(67,178,137,0.3)]",
+        hover: "border-primary-500",
+        error:
+          "border-interaction-error text-interaction-error shadow-[2px_2px_6px_0px_rgba(221,66,88,0.3)]",
+        warning: "border-interaction-warning text-interaction-warning",
+        valid: "border-interaction-success shadow-[2px_2px_6px_0px_rgba(114,200,59,0.3)]",
+        disabled: "border-neutral-600 text-neutral-600 bg-background cursor-not-allowed",
       },
     },
     defaultVariants: {
