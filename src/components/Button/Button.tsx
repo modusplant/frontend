e543d87adc591337f5911094c375fb7bc1e1e265
ejinn1 <ejinn1@naver.com>
2025-04-09 18:47:43 +0900

    MP-127-feat: create button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,17 @@
-function Button() {
-  return <div>버튼 컴포넌트</div>;
+import { buttonStyles } from "@/components/Button/buttonVariants";
+import { cn } from "@/utils/className";
+import { VariantProps } from "class-variance-authority";
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonStyles> {
+  children?: React.ReactNode;
 }
+
+const Button = ({ className, variant, size, ...props }: ButtonProps) => {
+  const buttonClass = cn(className, buttonStyles({ variant: variant, size: size }));
+
+  return <button className={buttonClass} disabled={variant === "disabled"} {...props} />;
+};
 
 export default Button;
