1c53af7def9d28e2b873af9ffe89e5ad158d7d9c
ejinn1 <ejinn1@naver.com>
2025-04-25 23:24:49 +0900

    MP-155-feat: singin page 퍼블리싱

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,10 +1,34 @@
+import SigninForm from "@/app/(auth)/signin/_components/SigninForm";
+import Image from "next/image";
+
 export const metadata = {
   title: "로그인",
   description: "로그인 페이지",
 };
 
-function Login() {
-  return <div>로그인 페이지</div>;
-}
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex h-screen w-screen flex-col items-center justify-center bg-white p-5 drop-shadow-md tablet:h-[672px] tablet:w-[640px] tablet:p-[50px]">
+        <Image src="/images/modus_plant_logo.png" width={233} height={100} alt="모두의식물 로고" />
 
-export default Login;
+        <SigninForm />
+
+        <div className="mt-10 flex items-center gap-5">
+          <button className="paragraph_small text-neutral-800">아이디 찾기</button>
+          <span> |</span>
+          <button className="paragraph_small text-neutral-800"> 비밀번호 재설정</button>
+        </div>
+
+        <div className="mt-[50px] flex items-center gap-[30px]">
+          <button>
+            <Image src="/img/kakao.png" width={45} height={45} alt="카카오 로그인 버튼" />
+          </button>
+          <button>
+            <Image src="/img/google.png" width={45} height={45} alt="구글 로그인 버튼" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
