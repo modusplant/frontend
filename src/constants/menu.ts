acab812cb17b10b0323101eb78afaab16d210ae2
JooKangSan <you04045@naver.com>
2025-06-27 20:24:59 +0900

    MP-216-feat: header 추가

---
+++ src/constants/menu.ts
@@ -0,0 +1,34 @@
+export const PAGE_LIST = [
+  {
+    title: "소통",
+    items: [
+      { label: "Q&A", href: "/community/qna" },
+      { label: "팁", href: "/community/tips" },
+      { label: "대화", href: "/community/chat" },
+    ],
+  },
+  {
+    title: "교환",
+    items: [
+      { label: "교환1", href: "/exchange/trade1" },
+      { label: "교환2", href: "/exchange/trade2" },
+      { label: "교환3", href: "/exchange/trade3" },
+    ],
+  },
+  {
+    title: "도전",
+    items: [
+      { label: "챌린지", href: "/challenge" },
+      { label: "도전1", href: "/challenge/challenge1" },
+      { label: "도전2", href: "/challenge/challenge2" },
+    ],
+  },
+  {
+    title: "정보",
+    items: [
+      { label: "정보1", href: "/info/info1" },
+      { label: "정보2", href: "/info/info2" },
+      { label: "정보3", href: "/info/info3" },
+    ],
+  },
+];
